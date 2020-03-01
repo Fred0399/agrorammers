30ea29cb84249ed7db0806c28762ec92101304b0
Rahman Mammadov <mammadli.kenta@gmail.com>
2020-03-01 23:36:21 +0400

    market foundations

--- lib/screens/market-page.dart
+++ lib/screens/market-page.dart
@@ -15,7 +15,39 @@ class MarketPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Bazar'),
       ),
-      body: Container(),
+      body: Container(
+        width: double.infinity,
+        child: Stack(
+          alignment: Alignment.bottomRight,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Image.asset(
+                'assets/catalog_items/Pomidor.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Image.asset(
+                'assets/catalog_items/Pomidor.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+// Container(
+//         height: 120.0,
+//         width: 120.0,
+//         decoration: new BoxDecoration(
+//           image: DecorationImage(
+//             image: new AssetImage('assets/catalog_items/Pomidor.png'),
+//             fit: BoxFit.cover,
+//           ),
+//           shape: BoxShape.circle,
+//         ),
